cbadced262a9c6b2a80dbef1e9456210e26f9eb4
IsThisMyRealName <IsThisMyRealName@users.noreply.github.com>
2018-11-24 18:09:45 +0100

    Merge pull my finger request

---
+++ frontEnd.js
@@ -0,0 +1,11 @@
+var numberOfDifferentNotes;
+var input = new Uint16Array(numberOfDifferentNotes);
+var input = new Uint16Array(numberOfDifferentNotes);
+var fps;
+
+setInterval(createBlocksFromInput, 1000/fps);
+
+function createBlocksFromInput()
+{
+    Console.Log("Test");
+}
